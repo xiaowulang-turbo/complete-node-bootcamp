246f3df46d7837dd4d1e7e263d7cf99a77277d8e
xiaowu <2728795232@qq.com>
2025-01-14 11:33:13 +0800

    feat(api): add JSON middleware and POST endpoint for tours

--- 4-natours/starter/app.js
+++ 4-natours/starter/app.js
@@ -3,6 +3,9 @@ const fs = require('fs');
 
 const app = express();
 
+// Middleware: function in the middle of request and response, can modify request and response
+app.use(express.json());
+
 // app.get('/', (req, res) => {
 //   res.status(200).json({
 //     message: 'Hello from the server side!',
@@ -25,6 +28,27 @@ app.get('/api/v1/tours', (req, res) => {
       tours,
     },
   });
+});
+
+app.post('/api/v1/tours', (req, res) => {
+  //console.log(req.body);
+  const id = tours[tours.length - 1].id + 1;
+
+  const newTour = Object.assign({ id }, req.body);
+  tours.push(newTour);
+
+  fs.writeFile(
+    `${__dirname}/dev-data/data/tours-simple.json`,
+    JSON.stringify(tours),
+    (err) => {
+      res.status(201).json({
+        status: 'success',
+        data: {
+          tour: newTour,
+        },
+      });
+    }
+  );
 });
 
 const port = 3000;
