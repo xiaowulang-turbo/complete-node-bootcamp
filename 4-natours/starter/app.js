8b7a39f23ab57707c17417b9cdff6ce76e41da28
xiaowu <2728795232@qq.com>
2025-01-14 14:44:40 +0800

    refactor(app.js): Extract route handlers to functions

--- 4-natours/starter/app.js
+++ 4-natours/starter/app.js
@@ -6,20 +6,12 @@ const app = express();
 // Middleware: function in the middle of request and response, can modify request and response
 app.use(express.json());
 
-// app.get('/', (req, res) => {
-//   res.status(200).json({
-//     message: 'Hello from the server side!',
-//     app: 'Natours',
-//   });
-// });
-
 const tours = JSON.parse(
   fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
 );
 // console.log(tours);
 
-// Only the callback function is running in event loop
-app.get('/api/v1/tours', (req, res) => {
+const getAllTours = (req, res) => {
   res.status(200).json({
     status: 'success',
     // only count when we have an array
@@ -28,10 +20,9 @@ app.get('/api/v1/tours', (req, res) => {
       tours,
     },
   });
-});
+};
 
-// The route should be matched exactly, use ? to make some params optional
-app.get('/api/v1/tours/:id/:x/:y?', (req, res) => {
+const getTour = (req, res) => {
   console.log(req.params);
 
   // a nice trick to convert string to number
@@ -54,9 +45,9 @@ app.get('/api/v1/tours/:id/:x/:y?', (req, res) => {
       tour,
     },
   });
-});
+};
 
-app.post('/api/v1/tours', (req, res) => {
+const createTour = (req, res) => {
   //console.log(req.body);
   const id = tours[tours.length - 1].id + 1;
 
@@ -75,9 +66,9 @@ app.post('/api/v1/tours', (req, res) => {
       });
     }
   );
-});
+};
 
-app.patch('/api/v1/tours/:id', (req, res) => {
+const updateTour = (req, res) => {
   if (req.params.id * 1 >= tours.length) {
     return res.status(404).json({
       status: 'fail',
@@ -91,7 +82,40 @@ app.patch('/api/v1/tours/:id', (req, res) => {
       tour: '<Updated tour here...>',
     },
   });
-});
+};
+
+const deleteTour = (req, res) => {
+  if (req.params.id * 1 >= tours.length) {
+    return res.status(404).json({
+      status: 'fail',
+      message: 'Invalid ID',
+    });
+  }
+
+  // 204: no content
+  res.status(204).json({
+    status: 'success',
+    data: null,
+  });
+};
+
+/*
+// Only the callback function is running in event loop
+app.get('/api/v1/tours', getAllTours);
+// The route should be matched exactly, use ? to make some params optional
+app.get('/api/v1/tours/:id/:x/:y?', getTour);
+app.post('/api/v1/tours', createTour);
+app.patch('/api/v1/tours/:id', updateTour);
+app.delete('/api/v1/tours/:id', deleteTour);
+*/
+
+app.route('/api/v1/tours').get(getAllTours).post(createTour);
+
+app
+  .route('/api/v1/tours/:id')
+  .get(getTour)
+  .patch(updateTour)
+  .delete(deleteTour);
 
 const port = 3000;
 app.listen(port, () => {
