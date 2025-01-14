6e653196dde1bb21948c9bdd36cb8ee878fefd1c
xiaowu <2728795232@qq.com>
2025-01-14 10:53:47 +0800

    feat(api): add tours data endpoint and JSON response

--- 4-natours/starter/app.js
+++ 4-natours/starter/app.js
@@ -1,11 +1,29 @@
 const express = require('express');
+const fs = require('fs');
 
 const app = express();
 
-app.get('/', (req, res) => {
+// app.get('/', (req, res) => {
+//   res.status(200).json({
+//     message: 'Hello from the server side!',
+//     app: 'Natours',
+//   });
+// });
+
+const tours = JSON.parse(
+  fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
+);
+// console.log(tours);
+
+// Only the callback function is running in event loop
+app.get('/api/v1/tours', (req, res) => {
   res.status(200).json({
-    message: 'Hello from the server side!',
-    app: 'Natours',
+    status: 'success',
+    // only count when we have an array
+    results: tours.length || 1,
+    data: {
+      tours,
+    },
   });
 });
 
