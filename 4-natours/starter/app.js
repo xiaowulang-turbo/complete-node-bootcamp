c57c7b7c0f96072160854a6cc70795e57daa6c4f
xiaowu <2728795232@qq.com>
2025-01-14 10:36:26 +0800

    feat: add initial server setup with express

---
+++ 4-natours/starter/app.js
@@ -0,0 +1,15 @@
+const express = require('express');
+
+const app = express();
+
+app.get('/', (req, res) => {
+  res.status(200).json({
+    message: 'Hello from the server side!',
+    app: 'Natours',
+  });
+});
+
+const port = 3000;
+app.listen(port, () => {
+  console.log('Server is running on port 3000...');
+});
