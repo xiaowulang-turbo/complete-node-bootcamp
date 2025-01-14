6b853b546e16eac113762e20e523c11b2f38ae58
xiaowu <2728795232@qq.com>
2025-01-14 13:02:01 +0800

    feat(api): add optional params and enhance tour retrieval

--- 4-natours/starter/app.js
+++ 4-natours/starter/app.js
@@ -23,9 +23,35 @@ app.get('/api/v1/tours', (req, res) => {
   res.status(200).json({
     status: 'success',
     // only count when we have an array
-    results: tours.length || 1,
+    results: tours?.length || 1,
     data: {
       tours,
+    },
+  });
+});
+
+// The route should be matched exactly, use ? to make some params optional
+app.get('/api/v1/tours/:id/:x/:y?', (req, res) => {
+  console.log(req.params);
+
+  // a nice trick to convert string to number
+  const id = req.params.id * 1;
+  const tour = tours.find((el) => el.id === id);
+
+  // variables made by const and let cannot be accessed before where they are declared
+  // if (id > tours.length) {
+  if (!tour) {
+    // use return to stop the function immediately
+    return res.status(404).json({
+      status: 'fail',
+      message: 'Invalid ID',
+    });
+  }
+
+  res.status(200).json({
+    status: 'success',
+    data: {
+      tour,
     },
   });
 });
